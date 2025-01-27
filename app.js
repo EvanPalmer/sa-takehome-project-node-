5a83d779c7e60bb29fb91f11a833cc22829b0fd5
Evan James <Evan James>
2025-01-27 17:08:39 +1100

    Set up Stripe hosted page integratoin

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 require('dotenv').config();
-const stripe = require('stripe');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const YOUR_DOMAIN = 'http://localhost:3000';
 
 var app = express();
 
@@ -11,6 +12,7 @@ app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
 }));
+
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }))
@@ -35,14 +37,17 @@ app.get('/checkout', function(req, res) {
     case '1':
       title = "The Art of Doing Science and Engineering"
       amount = 2300      
+      priceId = "price_1Ql0CLFSYQio1CHonUPjxlZ4"      
       break;
     case '2':
       title = "The Making of Prince of Persia: Journals 1985-1993"
       amount = 2500
+      priceId = "price_1QkyjGFSYQio1CHoE4NIjCY8"
       break;     
     case '3':
-      title = "Working in Public: The Making and Maintenance of Open Source"
+      title = "Working in Public: The Making and Maintenance of Open Source!"
       amount = 2800  
+      priceId = "price_1Ql0CwFSYQio1CHoXQr5XpVD"
       break;     
     default:
       // Included in layout view, feel free to assign error
@@ -53,8 +58,29 @@ app.get('/checkout', function(req, res) {
   res.render('checkout', {
     title: title,
     amount: amount,
+    priceId: priceId,
     error: error
   });
+});
+
+/**
+ * Create Checkout Session route
+ */
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: req.body.priceId,
+        quantity: 1, 
+      },
+    ], 
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success`,
+    cancel_url: `${YOUR_DOMAIN}/cancel`,
+});
+
+  res.redirect(303, session.url);
 });
 
 /**
@@ -62,6 +88,13 @@ app.get('/checkout', function(req, res) {
  */
 app.get('/success', function(req, res) {
   res.render('success');
+});
+
+/**
+ * Cancel route
+ */
+app.get('/cancel', function(req, res) {
+  res.render('cancel');
 });
 
 /**
